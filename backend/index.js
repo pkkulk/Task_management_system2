e50bc938cb268e99839d3e7d0a291604c8e94647
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-02-15 13:54:34 +0530

    l

--- backend/index.js
+++ backend/index.js
@@ -37,3 +37,6 @@ const PORT =process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log("server is running on port",PORT);
 });
+app.get("/", (req, res) => {
+    res.send("Task Management System API is running...");
+});
