828454670aea67ebdc3f41d06ac607e4c5644029
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-02-15 12:53:36 +0530

    hhsgs

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.options("*", cors(corsOptions)); 
+
 app.use(express.json());
 app.use("/api/TaskRoutes",T);
 mongoose.connect(process.env.MONGO_URI)
