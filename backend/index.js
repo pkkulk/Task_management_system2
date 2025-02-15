756e3e793a781a7379575e4d78fe2a88443f0602
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-02-15 13:37:11 +0530

    dd

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 
 const Task = require("./models/Task");
 const T =require("./Routes/TaskRoutes");
-const { default: mongoose } = require("mongoose");
   
 require("dotenv").config();
 const app=express();
@@ -28,11 +27,10 @@ app.use(
       credentials: true // Allow cookies if needed
     })
   );
-app.options("*", cors(corsOptions)); 
 
 app.use(express.json());
 app.use("/api/TaskRoutes",T);
-mongoose.connect(process.env.MONGO_URI)
+mongo.connect(process.env.MONGO_URI)
 .then(()=>{console.log("connected to mongoDB")} )
 .catch((error)=> console.log("Error connecting to mongoDB",error));
 const PORT =process.env.PORT || 5000;
